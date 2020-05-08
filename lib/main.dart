8de54d376f85c18b508e9f34dd3e124975641507
Amr ibrahim <you@example.com>
2020-05-08 08:21:36 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import './screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Course App',
+      theme: ThemeData(),
+      home: HomeScreen(),
+    );
+  }
+}
